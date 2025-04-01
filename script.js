d133fc1d81a8aca8ba8977b80c31eec53f78d33f
ameenstudio <200854608+ameenstudio@users.noreply.github.com>
2025-04-01 10:56:09 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let input=document.getElementById('inputF')
+let myTodolist
+
+
+
+function createListItems(, num) {
+    for (let i = 0; i < num; i++) {
+        let newLi = document.createElement('li')
+        parent.appendChild(newLi);
+    }
+
+}
