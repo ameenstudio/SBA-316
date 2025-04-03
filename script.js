b54f91210ff88da5952d711ddd5c8b1e3b7ae497
ameenstudio <200854608+ameenstudio@users.noreply.github.com>
2025-04-02 18:18:16 -0600

    mostly working missing a few things

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-let outerBox = document.querySelector('outerBox')
-let todoInput = document.querySelector('todoInput')
+
+let outerBox = document.querySelector('.outerBox')
+let todoInput = document.querySelector('#todoInput')
 let todoList = document.getElementById('todoList')
+let todoForm = document.getElementById('todoForm')
 
 
-function createListItems(, num) {
-    for (let i = 0; i < num; i++) {
-        let newLi = document.createElement('li')
-        parent.appendChild(newLi);
+
+todoForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addTodo(todoInput.value);
+    todoInput.value = "";
+});
+
+function addTodo(task) {
+    if (task === "") {
+        return; 
     }
 
+    let li = document.createElement("li")
+    li.textContent = task; 
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x"; // Setting the text of the button
+
+    li.appendChild(deleteBtn)
+
+    todoList.appendChild(li);
+
+    deleteBtn.addEventListener("click", function () {
+        li.remove(); 
+    });
+
+    li.addEventListener("click", function () {
+        li.classList.toggle("completed");
+    });
 }
